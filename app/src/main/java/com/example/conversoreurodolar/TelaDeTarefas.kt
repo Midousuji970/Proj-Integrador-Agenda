615a0c7705b85625d2cbdeb98d7fab8f15211548
Tacio <murilo_ts@live.com>
2023-09-13 19:52:02 -0300

    Quinta Commit(Adicicao de Fireabse)

--- app/src/main/java/com/example/conversoreurodolar/TelaDeTarefas.kt
+++ app/src/main/java/com/example/conversoreurodolar/TelaDeTarefas.kt
@@ -6,6 +6,7 @@ import com.example.conversoreurodolar.databinding.ActivityTelaDeTarefasBinding
 
 class TelaDeTarefas : AppCompatActivity() {
     private lateinit var binding: ActivityTelaDeTarefasBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityTelaDeTarefasBinding.inflate(layoutInflater)
